82ca1cd5925528f9a24335d9d170f629dd40eff0
tirumalasrinidhi <srinidhitirumala950@gmail.com>
2024-05-06 19:35:47 +0530

    Add files via upload

---
+++ temperature1.py
@@ -0,0 +1,21 @@
+celsius = 47
+fahrenheit = (celsius * 1.8) + 32
+print('%.2f Celsius is equivalent to: %.2f Fahrenheit'
+	% (celsius, fahrenheit))
+celsius = float(input("Enter temperature\
+in celsius: "))
+
+fahrenheit = (celsius * 1.8) + 32
+
+print(str(celsius )+ " degree Celsius\
+is equal to " + str(fahrenheit )+
+	" degree Fahrenheit.")
+fahrenheit = float(input("Enter temperature in fahrenheit: "))
+
+celsius = (fahrenheit - 32)/1.8
+
+print(str(fahrenheit )+ " degree Fahrenheit is equal\
+to " + str(celsius ) + " degree Celsius." )
+
+
+
